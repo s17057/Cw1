174a8fb60dea639b2a522165e518d7c67927e2d7
Dominik Domagalski <s17057@pjwstk.edu.pl>
2020-03-01 12:52:59 +0100

    s

--- Cw1/Cw1/Program.cs
+++ Cw1/Cw1/Program.cs
@@ -1,16 +1,34 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Cw1
 {
     class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             //Console.WriteLine("Hello World!");
 
-            var httpClient = new HttpClient();
-            var response = await httpClient.GetAsync(args[0]);
+            var client = new HttpClient();
+            var result = await client.GetAsync("https://www.pja.edu.pl");
+            if (result.IsSuccessStatusCode)
+            {
+                string html = await result.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]",
+                RegexOptions.IgnoreCase);
+
+                MatchCollection matches = regex.Matches(html);
+                foreach (var m in matches)
+                {
+                    Console.WriteLine(m.ToString());                }
+            }
+
+            // var response = await httpClient.GetAsync(args[0]);
+
+            Console.WriteLine("END!");
+
         }
     }
 }
